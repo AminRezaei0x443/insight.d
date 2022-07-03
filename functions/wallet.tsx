ed4bf2510198d2d0cf2275751f6d78f8120f8032
Shayan Shojaei <shayanshojaei.2002@gmail.com>
2022-07-03 15:43:57 +0430

    feat: setup wallet connection

---
+++ functions/wallet.tsx
@@ -0,0 +1,45 @@
+import TonWeb from "tonweb";
+
+export let ton: TonWeb | undefined = undefined;
+export let wallet: any = undefined;
+
+export const initializeWalletHelper = () => {
+    // @ts-ignore
+    ton = new TonWeb(window.ton);
+}
+
+//@ts-ignore
+export const isPluginSupported = () => ton !== undefined && window.ton;
+
+export const connectWallet = async (publicKey: string | null): Promise<boolean> => {
+    if (!publicKey) {
+        const accounts = await ton?.provider.send("ton_requestAccounts", undefined);
+        if (!Array.isArray(accounts) || accounts.length === 0)
+            return false;
+        publicKey = accounts[0]
+        window.localStorage.setItem("pk", publicKey ?? "");
+        console.log(publicKey);
+    }
+
+
+    wallet = ton?.wallet.create({ publicKey });
+    console.log(wallet);
+
+    return true;
+}
+
+export const disconnectWallet = async (): Promise<boolean> => {
+    window.localStorage.removeItem("pk")
+    wallet = undefined;
+    return true;
+}
+
+export const isConnected = () => wallet !== undefined;
+
+export const getBalance = async (): Promise<string> => {
+    const balance = await ton?.provider.send('ton_getBalance', undefined);
+    if (typeof balance === "string") {
+        return TonWeb.utils.fromNano(balance);
+    }
+    return "0";
+}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 module.exports = {
     images: {
         domains: ["a.thumbs.redditmedia.com"]
-    }
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        
+        config.module.rules.push({
+            test: /\.js$/,
+            exclude: /node_modules\/(?!tonweb\/).*/,
+            loader: defaultLoaders.babel,
+        });
+
+        // Important: return the modified config
+        return config;
+    },
 }
